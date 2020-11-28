e191424ceec1d3fd60de9b4440e9ebe7625e69cb
chaim-chv <chaimvaya@gmail.com>
2020-11-28 18:47:09 +0200

    adding 'dark theme' support

--- background.js
+++ background.js
@@ -3,3 +3,10 @@ chrome.browserAction.onClicked.addListener(function () {
     browser.tabs.duplicate(tabs[0].id);
     })
 });
+
+window.onload = function () {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  chrome.browserAction.setIcon({
+    path: "icons-dark/dup24dark.png"
+  });
+} }
