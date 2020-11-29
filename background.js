8b97d72938bf5babf1299936aa79f2f9ae2710bf
chaim-chv <chaimvaya@gmail.com>
2020-11-29 17:42:00 +0200

    format fix

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 chrome.browserAction.onClicked.addListener(function () {
   chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
     browser.tabs.duplicate(tabs[0].id);
-    })
+  });
 });
 
 window.onload = function () {
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  chrome.browserAction.setIcon({
-    path: "icons-dark/dup24dark.png"
-  });
-} }
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    chrome.browserAction.setIcon({
+      path: "icons-dark/dup24dark.png",
+    });
+  }
+};
